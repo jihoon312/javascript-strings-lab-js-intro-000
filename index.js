fae215cbe26c48adfe80fff0fdb83a39dd626e2f
Ji Hoon Park <jihoon312@gmail.com>
2020-10-09 18:08:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var greeting = "Hello, everybody!"
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello, everybody, " + specialGuest + "!";
+var greetSpecialGuest = "Hello everybody, " + specialGuest + "!";
 
 var topic = "space";
 
 var conversation = `${topic}`;
+learn
